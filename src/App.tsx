35b4b0da57d65336cef3d6fdf3f919a257fa91a5
gopal singh <gopalsingh@gopals-MacBook-Air.local>
2025-06-26 20:00:47 +0530

    ✅ Set up manual Firebase deploy workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { HashRouter } from "react-router-dom"; // ✅ Use HashRouter
 import MainRount from './componest/MainRounts/MainRount';
-import "./app.css"
+
 function App() {
   return (
     <HashRouter>
